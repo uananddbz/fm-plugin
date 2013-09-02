a591ec9c72f95e2a2b3a0163d2bce1c89f1dfd22
utkarsh anand <uananddbz@gmail.com>
2013-09-02 15:56:47 +0530

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+<script src="fm.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$("#demo").fm("demo_folder");
+});
+</script>
+</head>
+<body>
+<h1> file manager</h1>
+<div id="demo"> result </div>
+</body>
+</html>
